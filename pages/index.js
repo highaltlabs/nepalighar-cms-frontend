85fbf10ea0f9928e53f49a40a930956629fcaa9d
Seema Thapa <tseema@insightworkshop.io>
2021-07-20 15:43:57 +0545

    add videos options

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
             <div className="relative flex items-center justify-center overflow-hidden" style={{height: '60vh'}}>
                 <video autoPlay loop muted className="absolute z-10 w-auto min-w-full min-h-full max-w-none">
                     <source
-                        src="https://assets.mixkit.co/videos/preview/mixkit-set-of-plateaus-seen-from-the-heights-in-a-sunset-26070-large.mp4"
+                        src={'/baudha.mp4'}
                         type="video/mp4"/>
                     Your browser does not support the video tag.
                 </video>
